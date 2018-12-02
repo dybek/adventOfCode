e9e3be98ee5c4bc776051bf7db6d070ea32bf8e6
tdybowski <t.dybowski@gmail.com>
2018-12-02 15:58:36 +0100

    Modify read input function

--- 2018/day2/app.js
+++ 2018/day2/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 
 class InputRow {
@@ -73,7 +74,8 @@ class Day {
 
 };
 
-function fileInput(file){
+function fileInput(fileName){
+    const file = path.join(__dirname, fileName)
     return fs.readFileSync(file, { encoding: 'utf-8' })
         .split('\n')
         .map(row => new InputRow(row));
