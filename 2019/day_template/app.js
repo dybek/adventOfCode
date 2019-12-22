62fc79786ec7b94a2ea7448d5f0ad17db58167fb
tdybowski <t.dybowski@gmail.com>
2019-12-22 02:08:58 +0100

    Add day template

---
+++ 2019/day_template/app.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+class InputRow {
+
+    constructor(row) {
+        this.value = row;
+    }
+}
+
+class DayX {
+    constructor(dayInput) {
+        this.dayInput = dayInput;
+        console.log(this.part1());
+        console.log(this.part2());
+    }
+
+    part1() {
+        
+    }
+
+    part2() {
+  
+    }
+
+};
+
+function fileInput(fileName){
+    const file = path.join(__dirname, fileName)
+    return fs.readFileSync(file, { encoding: 'utf-8' })
+        .split('\n')
+        .map(row => new InputRow(row));
+}
+function day(file){
+    new Day1(fileInput(file));
+}
+
+day('data.input');
+day('data_test.input');
