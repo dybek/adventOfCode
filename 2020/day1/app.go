fd7d992e08a362971743bc48297bc2d277e92e90
Tomasz Dybowski <t.dybowski@gmail.com>
2020-12-26 01:19:53 +0100

    Day 1 2020 Completed

--- 2020/day1/app.go
+++ 2020/day1/app.go
@@ -3,15 +3,63 @@ package main
 import "fmt"
 import "io/ioutil"
 import "strings"
+import "strconv"
 
-func main() {
-	fmt.Println(day1("input.data"))
+func day1(content []byte) {
+	input := strings.Split(string(content), "\n")
+
+	var numbers = make([]int, len(input))
+	var err error
+	for i := 0; i < len(numbers); i++ {
+		numbers[i], err = strconv.Atoi(input[i])
+	}
+	if err != nil {
+		panic(err)
+	}
+
+
+	fmt.Println(part1(numbers))
+	fmt.Println(part2(numbers))
 }
 
-func day1(fileName string) []string {
+func part1(numbers []int) int{
+	for i := 0; i < len(numbers); i++ {
+		for j := 0; j < len(numbers); j++ {
+			if numbers[i] + numbers[j] == 2020{
+				return numbers[i] * numbers[j]
+			}
+		}
+	}
+	return 0
+}
+
+func part2(numbers []int) int{
+	for i := 0; i < len(numbers); i++ {
+		for j := 0; j < len(numbers); j++ {
+			for k := 0; k < len(numbers); k++ {
+				if numbers[i] + numbers[j] + numbers[k] == 2020{
+					return numbers[i] * numbers[j] * numbers[k]
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func fileInput(fileName string) []byte{
+
 	content, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
-	return strings.Split(string(content), "\n\r")
+	return content
+}
+
+func day(file string) {
+    day1(fileInput(file))
+}
+
+func main() {
+	day("input.data")
+	day("input2.data")
 }
